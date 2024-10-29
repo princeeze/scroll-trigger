b7ddf10850f19f9ba7ce1cd3855488afba51da3d
Prince Eze <pchidireeze@gmail.com>
2024-10-29 20:41:39 +0100

    feat: add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const ppNeue = localFont({
+  src: "./fonts/PPNeueMontreal.otf",
+  variable: "--font-ppneue",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${ppNeue.variable} font-sans antialiased`}>
         {children}
       </body>
     </html>
