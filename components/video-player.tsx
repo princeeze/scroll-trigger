d2211a167e72646f2d52cefb33a668495dcb6f39
Prince Eze <pchidireeze@gmail.com>
2024-10-31 12:00:36 +0100

    fix: make videojs dynamically imported to avoid running server side
    video js tries to access the window component in server environments after import

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -1,40 +1,44 @@
 // components/VideoPlayer.tsx
 import { useEffect, useRef } from "react";
-import videojs from "video.js";
-import "video.js/dist/video-js.css";
-import "videojs-contrib-dash";
 import Player from "video.js/dist/types/player";
+import "video.js/dist/video-js.css";
 
 export default function VideoPlayer({ src }: { src: string }) {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const playerRef = useRef<Player | null>(null);
 
   useEffect(() => {
-    if (!playerRef.current) {
-      const videoNode = videoRef.current;
+    const initPlayer = async () => {
+      const videojs = (await import("video.js")).default;
 
-      if (videoNode) {
-        playerRef.current = videojs(videoNode, {
-          controls: false,
-          autoplay: true,
-          preload: "auto",
-          muted: true,
-          fluid: false,
-          responsive: true,
-          poster: "/poster.png",
-          bigPlayButton: false,
-          loadingSpinner: false,
-          loop: true,
-          fill: true,
-          sources: [
-            {
-              src: src,
-              type: "application/dash+xml", // For DASH streams
-            },
-          ],
-        });
+      if (!playerRef.current) {
+        const videoNode = videoRef.current;
+
+        if (videoNode) {
+          playerRef.current = videojs(videoNode, {
+            controls: false,
+            autoplay: true,
+            preload: "auto",
+            muted: true,
+            fluid: false,
+            responsive: true,
+            poster: "/poster.png",
+            bigPlayButton: false,
+            loadingSpinner: false,
+            loop: true,
+            fill: true,
+            sources: [
+              {
+                src: src,
+                type: "application/dash+xml", // For DASH streams
+              },
+            ],
+          });
+        }
       }
-    }
+    };
+
+    initPlayer();
 
     // Cleanup function to dispose of Video.js player
     return () => {
