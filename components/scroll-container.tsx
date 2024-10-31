20803dca40880d4caae5abfc9dc17e705f03ca10
Prince Eze <pchidireeze@gmail.com>
2024-10-31 09:06:27 +0100

    fix: use videojs component and transform viewport height rather than width

--- components/scroll-container.tsx
+++ components/scroll-container.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
+import VideoPlayer from "./video-player";
 
 export function ScrollContainer() {
   const videoDiv = useRef<HTMLDivElement>(null);
@@ -10,18 +11,24 @@ export function ScrollContainer() {
     offset: ["40% end", "end end"],
   });
 
-  const widthLevel = useTransform(scrollYProgress, [0, 1], ["50vw", "100vw"]);
+  const heightLevel = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ["40dvh", "100dvh"]
+  );
 
   return (
     <div
       ref={videoDiv}
       className="h-[150dvh] relative w-full flex justify-center bg-black"
     >
-      <motion.div
-        className=" sticky h-[100dvh] w-full p-[1vw] flex  top-0 "
-        style={{ width: widthLevel }}
-      >
-        <video src="/video.mp4" className="w-full" autoPlay muted loop></video>
+      <motion.div className=" sticky h-[100dvh] w-full flex  top-0 justify-center items-center ">
+        <motion.div
+          className="w-full p-[1dvh] flex justify-center items-center"
+          style={{ height: heightLevel }}
+        >
+          <VideoPlayer src="https://zgedlwpfczwztvxbgywz.supabase.co/storage/v1/object/public/video/-ed85-43aa-86d5-96d4b88caa8c.mpd" />
+        </motion.div>
       </motion.div>
     </div>
   );
